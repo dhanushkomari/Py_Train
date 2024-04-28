4b0e8e6a9d8c3855118eb4010f887097c08a1a6d
dhanushkomari <dhanushkomari@gmail.com>
2024-04-28 17:13:49 +0530

    Exception Handling

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
-import math
+def addition(a, b):
+    """This addtion function will help us to find the addition of given two numbers
+       This function will take two integers or floating numbers as input
+    """
 
-print(math.sqrt(9))
+    print("The addition result is :",a+b)
 
-print(math.factorial(5))
+def multiplication(a, b):
+    print("Mul of ", a, b, "is: ")
+    print(a*b)
+    return a*b
